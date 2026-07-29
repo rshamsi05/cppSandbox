e74ad7be5a0ad0fed08bde18c0fff2eab9d740a7
Rayan Shamsi <shamsirayan16@gmail.com>
2026-07-29 13:02:31 -0400

    Finished implementing hashmap core functions.

--- Scratch/CustomHashMap/src/linear_probed_map.hpp
+++ Scratch/CustomHashMap/src/linear_probed_map.hpp
@@ -4,6 +4,7 @@
 #include <functional>
 #include <optional>
 #include <vector>
+#include <stdexcept>
 #include "state_node.hpp"
 
 
@@ -32,14 +33,36 @@ class LinearProbedMap {
             table.resize(capacity);
         }
     
-    // Private helper method that computes the index based on the hash function: n%size
     private:
+        // Private helper method that computes the index based on the hash function: n%size
         size_t getIndex(const K& key, size_t cap) const {
             return std::hash<K>{}(key) % cap;
         }
-
+        // Private helper method that resizes the current table to be 2*capacity
         void resize(size_t new_capacity) {
-            // logic to resize the table
+            // create new table with bigger capacity and resets the values of capacity and num_elements.
+            std::vector<HashNode<K,V>> new_table = std::vector<HashNode<K,V>>(new_capacity);
+            capacity = new_capacity;
+            num_elements = 0;
+            // transfer data over
+            for(const auto& node: table){
+                if(node.state == SlotState::OCCUPIED){
+                    size_t startIndex = getIndex(node.key, new_capacity);
+
+                    // Linearly probe
+                    for(size_t i = 0; i < new_capacity; ++i){
+                        size_t currIndex = (startIndex + i)%new_capacity;
+                        if(new_table[currIndex].state == SlotState::EMPTY){
+                            new_table[currIndex].key = node.key;
+                            new_table[currIndex].value = node.value;
+                            new_table[currIndex].state = SlotState::OCCUPIED;
+                            num_elements++;
+                            break;
+                        }
+                    }
+                }
+            }
+            table = std::move(new_table);
         }
     
 
@@ -65,6 +88,7 @@ class LinearProbedMap {
                     // checks if we are updating an existing key in the map, otherwise we continue to next slot
                     if(table[currIndex].key == key){
                         table[currIndex].value = value;
+                        return; // we can return out of the function after we simply update the value
                     }
                 }
                 // case where state is DELETED
@@ -98,11 +122,44 @@ class LinearProbedMap {
         }
    
         std::optional<V> get(const K& key) const {
-            
+            // get index of the key
+            size_t startIndex = getIndex(key, capacity);
+            // probe for space with the same key
+            for(size_t i = 0; i < capacity; ++i){
+                size_t currIndex = (startIndex+i)%capacity;
+                // if the slot where we lookup the key is empty, the key does not exist in the map and we must return an exception.
+                if(table[currIndex].state == SlotState::EMPTY){
+                    throw std::out_of_range("Key does not exist in the hashmap!");
+                }
+                // if the state of the slot is deleted, then we skip it
+                if(table[currIndex].state == SlotState::DELETED){
+                    continue;
+                }
+                // if the state of the slot is occupied and the keys match, we found the value we want to return.
+                if(table[currIndex].state == SlotState::OCCUPIED && table[currIndex].key == key){
+                    return table[currIndex].value;
+                }
+            }
+            // if loop exits, key was not found in map and we throw an out of range exception.
+            throw std::out_of_range("Key does not exist in the hashmap!");
+
         }
     
         bool remove(const K& key) {
-
+            size_t startIndex = getIndex(key, capacity);
+            for(size_t i = 0; i < capacity; ++i){
+                size_t currIndex = (startIndex+i)%capacity;
+                // if the slot where we lookup the key is empty, the key does not exist in the map and we must return false.
+                if(table[currIndex].state == SlotState::EMPTY){
+                    return false;
+                }
+                if(table[currIndex].state == SlotState::OCCUPIED && table[currIndex].key == key){
+                    table[currIndex].state = SlotState::DELETED;
+                    num_elements--;
+                    return true;
+                }
+            }
+            return false;
         }
 
 };
