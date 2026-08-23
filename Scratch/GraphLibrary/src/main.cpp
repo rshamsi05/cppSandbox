8baf67723e25d2405e294a2a1359e32cf5617988
Rayan Shamsi <shamsirayan16@gmail.com>
2026-08-23 15:24:50 -0400

    Added testing file for graph library.

--- Scratch/GraphLibrary/src/main.cpp
+++ Scratch/GraphLibrary/src/main.cpp
@@ -1,6 +1,36 @@
+#include "graph.hpp"
+#include "graph_traversals.hpp"
 #include <iostream>
 
 int main() {
-  std::cout << "Starter Code." << std::endl;
+  Graph<std::string> graph1;
+  graph1.add_vertex("john");
+  graph1.add_vertex("joe");
+  graph1.add_vertex("sean");
+  graph1.add_vertex("bob");
+  graph1.add_edge("john", "joe");
+  graph1.add_edge("john", "sean");
+  graph1.add_edge("joe", "bob");
+  graph1.add_edge("sean", "bob");
+  std::cout << "Johns neighbors: ";
+  std::vector<std::string> johnNeighbors = graph1.getNeighbors("john");
+  for(const auto& neighbor: johnNeighbors){
+    std::cout << neighbor << ", ";
+  }
+
+  std::cout << std::endl;
+  std::cout << "bfs on john: ";
+  std::vector<std::string> bfsOnJohn = GraphTraversals::bfs<std::string>(graph1, "john");
+  for(const auto& node: bfsOnJohn){
+    std::cout << node << ", ";
+  }
+
+
+  std::cout << std::endl;
+  std::cout << "dfs on john: ";
+  std::vector<std::string> dfsOnJohn = GraphTraversals::dfs<std::string>(graph1, "john");
+  for(const auto& node: dfsOnJohn){
+    std::cout << node << ", ";
+  }
   return 0;
 }
